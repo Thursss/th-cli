f2a0ccfc149ca9b16fed56ede31c65f588dbbbf9
thursss <1720557879@qq.com>
2023-01-26 21:01:07 +0800

    feat: 提供基础的git模板下载功能

--- bin/cli.js
+++ bin/cli.js
@@ -2,21 +2,16 @@
 
 const program = require('commander')
 
+// 版本号
+program.version(require('../package.json').version, '-v', 'version')
+
 program
 	// 定义命令和参数
-	.command('create [name]')
-	.description('create a new project')
-	// -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-	.option('-f, --force', 'overwrite target directory if it exist')
-	.action((name, options) => {
-		// 打印结果，输出用户手动输入的项目名字
-		console.log('name:', name)
+	.command('init <type>')
+	.description('创建新项目')
+	.action((type) => {
+		require('../lib/init')(type)
 	})
 
-program
-	// 配置版本号信息
-	.version(`v${require('../package.json').version}`)
-	.usage('<command> [option]')
-
-// 解析用户执行命令传入参数
+// 解析命令行
 program.parse(process.argv)
